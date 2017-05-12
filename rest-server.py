6c6f66908a759dd8c77b709dc5d0c32a2e5c01b7
Riccardo Corbella <739397@gmail.com>
2017-05-12 13:35:56 +0000

    add registration to api

--- rest-server.py
+++ rest-server.py
@@ -7,10 +7,19 @@ def index(user):
     return template('<b>Hi {{user}}! How are you doing? </b>!', user=user)
 
 # user registration, the input is a json
+# curl --data "{\"name\":\"riccardo\", \"surname\":\"corbella\", \"dateofbirth\":\"1989-12-02\", \"residence\":\"milano\"}" localhost:8081/register
 @post('/register')
 def signup():
 	data=json.loads(request.body.read())	
 	c = Connection(host='127.0.0.1', port=8080)
 	t = c.table('anagrafica')
-	d.insert(data['name'], { 'info': {'name': data['name']}})
+	t.insert(data['name'],
+		{
+			'info': {'name': data['name']},
+			'info': {'surname': data['surname']},
+			'info': {'dateofbirth': data['dateofbirth']},
+			'info': {'residence': data['residence']}
+		}
+	)
+
 run(host='localhost', port=8081)
